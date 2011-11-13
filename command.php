391bb85dd005242592029d351944a6a6f340312d
Helen <hell@hell-N150P-N210P-N220P.(none)>
2011-11-13 11:51:36 +0000

    added ar drone stuff

--- command.php
+++ command.php
@@ -5,18 +5,23 @@ $command = $_GET['command'];
 
 switch ($command) {
 	case "FORWARD":
+		system("/home/hell/mindbodysoul/ardrone/interface forward");
 		system($dir."forward");
 		break;
 	case "BACK":
+		system("/home/hell/mindbodysoul/ardrone/interface back");			
 		system($dir."back");
 		break;
 	case "LEFT":
+		system("/home/hell/mindbodysoul/ardrone/interface left");
+
 		system($dir."left");
 		break;
 	case "LEFT45":
 		system($dir."left45");
 		break;
 	case "RIGHT":
+		system("/home/hell/mindbodysoul/ardrone/interface right");
 		system($dir."right");
 		break;
 	case "RIGHT45":
@@ -25,7 +30,11 @@ switch ($command) {
 	case "VERIFY":
 		echo "VERIFIED\r\n<br>I'm a robot!";
 		break;
+	case "STOP":
+		system("/home/hell/mindbodysoul/ardrone/interface stop");
+		break;
 	default:
+		system("/home/hell/mindbodysoul/ardrone/interface stop");
 		echo "Command invalid. Go away.";
 		break;
 }
